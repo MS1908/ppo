9e385bee083eb8009ff8d2ef8acf23e403a645e2
haitt00 <54594085+haitt00@users.noreply.github.com>
2019-12-25 23:09:14 +0700

    complete observation_space

--- discrete/envs/offload_autoscale_env.py
+++ discrete/envs/offload_autoscale_env.py
@@ -15,12 +15,18 @@ class OffloadAutoscaleDiscreteEnv(gym.Env):
 
         self.lamda_high = 100  # units/second
         self.lamda_low = 10
-        self.b_high = self.batery_capacity / self.timeslot  # W
+        self.lamda_step = 10
+        self.lamda_n = round((self.lamda_high - self.lamda_low) / self.lamda_step + 1)
+        self.b_high = round(self.batery_capacity / self.timeslot)  # W
         self.b_low = 0
-        self.e_low = 0
-        self.h_high = 60  # ms/unit
-        self.h_low = 20
+        self.b_n = self.b_high + 1
+        self.h_high = 0.06  # ms/unit
+        self.h_low = 0.02
+        self.h_step = 0.01
+        self.h_n = round((self.h_high - self.h_low) / self.h_step + 1)
         self.e_high = 2
+        self.e_low = 0
+        self.e_n = self.e_high + 1
 
         self.back_up_cost_coef = 0.15
         self.normalized_unit_depreciation_cost = 0.01
@@ -43,13 +49,23 @@ class OffloadAutoscaleDiscreteEnv(gym.Env):
             self.b_low,
             self.h_low,
             self.e_high])
-        self.observation_space = spaces.MultiDiscrete([self.lamda_high + 1, self.b_high + 1, self.h_high + 1, self.e_high + 1])
-        self.action_space = spaces.Discrete(int(self.b_high))
+        self.observation_space = spaces.MultiDiscrete([self.lamda_n, self.b_n, self.h_n, self.e_n])
+        self.action_space = spaces.Discrete(self.b_n)
         self.state = [0, 0, 0, 0]
 
         # self.state.append(np.array(obs[1]))
         self.time = 0
         self.episode = 0
+    def de_lamda(self, lamda):
+        return self.lamda_low + self.lamda_step * lamda
+    def de_h(self, h):
+        return self.h_low + self.h_step * h
+    def de_state(self, state):
+        lamda, b, h, e = state
+        lamda = self.de_lamda(lamda)
+        h = self.de_h(h)
+        state = np.array([lamda, b, h, e])
+        return state
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -70,10 +86,10 @@ class OffloadAutoscaleDiscreteEnv(gym.Env):
         return np.random.poisson(800)
 
     def get_lambda(self):
-        return np.random.random_integers(self.lamda_low, self.lamda_high)
+        return np.random.randint(self.lamda_n)
 
     def get_h(self):
-        return np.random.random_integers(self.h_low, self.h_high)
+        return np.random.randint(self.h_n)
 
     def get_time(self):
         self.time += 0.25
@@ -155,19 +171,20 @@ class OffloadAutoscaleDiscreteEnv(gym.Env):
 
     def step(self, action):
         done = False
-        action = int(action)
         # self.time_step += 1
         self.get_time()
         state = self.state
-        # print('\tstate: ',state)
-        # print('\ttime: ',self.time)
+        print('\tstate: ',state)
+        print('\ttime: ',self.time)
+        state = self.de_state(state)
+        print('\tde_state: ',state)
         g_t = self.get_g(state[3])
-        # print('\tget ', g_t)
-        # print('\taction: ', action)
+        print('\tget ', g_t)
+        print('\taction: ', action)
         d_op, d_com, d, number_of_server, local_workload, done = self.power_model(action)
         # print('\t{0:10}{1:10}{2:10}{3:20}{4:10}'.format('d_op','d_com','d','number_server','local_workload'))
         # print('\t{0:<10.3f}{1:<10.3f}{2:<10.3f}{3:<20.3f}{4:<10.3f}'.format(d_op, d_com, d, number_of_server, local_workload))
-        reward = 1 / self.reward_func(action, g_t, d_op, d, number_of_server, local_workload)
+        reward = self.reward_func(action, g_t, d_op, d, number_of_server, local_workload)
         cost_delay_local = self.cost_delay_local_function(number_of_server, local_workload)
         cost_delay_cloud = self.cost_delay_cloud_function(local_workload, state[2], state[0])
         lambda_t = self.get_lambda()
@@ -175,8 +192,8 @@ class OffloadAutoscaleDiscreteEnv(gym.Env):
         h_t = self.get_h()
         e_t = self.get_e()
         self.state = np.array([lambda_t, b_t, h_t, e_t])
-        # print('\tnew state: ', self.state)
-        # print('\treward: ', reward)
+        print('\tnew state: ', self.state)
+        print('\treward: ', reward)
         if b_t < 0 or (cost_delay_cloud < 0 and cost_delay_local < 0) or reward < 0:
             done = True
             reward = 1e18
@@ -184,7 +201,7 @@ class OffloadAutoscaleDiscreteEnv(gym.Env):
         return self.state, reward, done, {}
 
     def reset(self):
-        self.state = np.array([self.lamda_low, self.b_low, self.h_low, self.e_low])
+        self.state = np.array([0, 0, 0, 0])
         self.time_step = 0
         self.time = 0
         return self.state
@@ -195,12 +212,13 @@ if __name__ == '__main__':
     # # obs = MyEnv.observation_space.sample()
     # # print('debug: ', obs)
     # # print(MyEnv.observation_space.sample())
-    for i in range(1000):
+    for i in range(24*4):
         print('STEP: ', i)
         action = MyEnv.action_space.sample()
         state, reward, done, _ = MyEnv.step(action)
         if done:
             MyEnv.reset()
+
     #     print('STEP: ', i)
     #     state, reward = MyEnv.step(i+3000)
     #     # state, reward = MyEnv.step(MyEnv.state[1])
