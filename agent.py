9f48382a5d4be7dcabb2ce2b4f8f07f5561541b9
haitt00 <54594085+haitt00@users.noreply.github.com>
2020-02-04 23:43:10 +0700

    .

--- agent.py
+++ agent.py
@@ -1,7 +1,9 @@
+
 import gym
 import gym_offload_autoscale
 import numpy as np
 import pandas as pd
+import os
 
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
@@ -13,7 +15,7 @@ env = gym.make('offload-autoscale-v0')
 env = DummyVecEnv([lambda: env])
 
 model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=100000)
+model.learn(total_timesteps=1000)
 # print(env.env_method('fixed_action_cal', 1000))
 # exit()
 rewards_list_ppo = []
@@ -26,43 +28,64 @@ rewards_list_fixed_1 = []
 avg_rewards_fixed_1 = []
 rewards_list_fixed_2 = []
 avg_rewards_fixed_2 = []
-obs = env.reset()
+rand_seed = 1234
+s = 2
+t_range = 100
 
-t_range = 10000
+env.env_method('seed', rand_seed)
+np.random.seed(rand_seed)
+os.environ['PYTHONHASHSEED']=str(rand_seed)
+obs = env.reset()
 for i in range(t_range):
     action = env.env_method('myopic_action_cal')
     obs, rewards, dones, info = env.step(action)
-    rewards_list_myopic.append(1 / rewards)
+    rewards_list_myopic.append(1 / rewards/ s)
     avg_rewards_myopic.append(np.mean(rewards_list_myopic[:]))
     if dones: env.reset()
 
+env.env_method('seed', rand_seed)
+np.random.seed(rand_seed)
+os.environ['PYTHONHASHSEED']=str(rand_seed)
+obs = env.reset()
 for i in range(t_range):
     action = env.env_method('fixed_action_cal', 400)
-    # action = [0]
     obs, rewards, dones, info = env.step(action)
-    rewards_list_fixed_1.append(1 / rewards)
+    rewards_list_fixed_1.append(1 / rewards/ s)
     avg_rewards_fixed_1.append(np.mean(rewards_list_fixed_1[:]))
     if dones: env.reset()
 
+env.env_method('seed', rand_seed)
+np.random.seed(rand_seed)
+os.environ['PYTHONHASHSEED']=str(rand_seed)
+obs = env.reset()
 for i in range(t_range):
     action = env.env_method('fixed_action_cal', 1000)
     obs, rewards, dones, info = env.step(action)
-    rewards_list_fixed_2.append(1 / rewards)
+    rewards_list_fixed_2.append(1 / rewards/ s)
     avg_rewards_fixed_2.append(np.mean(rewards_list_fixed_2[:]))
     if dones: env.reset()
 
-for i in range(t_range):
-	action = np.random.uniform(0, 1, 1)
-	obs, rewards, dones, info = env.step(action)
-	rewards_list_random.append(1 / rewards)
-	avg_rewards_random.append(np.mean(rewards_list_random[:]))
-	if dones: env.reset()
-
+env.env_method('seed', rand_seed)
+np.random.seed(rand_seed)
+os.environ['PYTHONHASHSEED']=str(rand_seed)
+obs = env.reset()
 for i in range(t_range):
     action, _states = model.predict(obs)
     # action = np.random.uniform(0, 1, 1)
     obs, rewards, dones, info = env.step(action)
-    rewards_list_ppo.append(1 / rewards)
+    rewards_list_random.append(1 / rewards/ s)
+    avg_rewards_random.append(np.mean(rewards_list_random[:]))
+    if dones: env.reset()
+
+env.env_method('seed', rand_seed)
+np.random.seed(rand_seed)
+os.environ['PYTHONHASHSEED']=str(rand_seed)
+obs = env.reset()
+for i in range(t_range):
+    action, _states = model.predict(obs)
+    # action = np.random.uniform(0, 1, 1)
+    obs, rewards, dones, info = env.step(action)
+    rewards_list_ppo.append(1 / rewards/ s)
     avg_rewards_ppo.append(np.mean(rewards_list_ppo[:]))
     if dones: env.reset()
     # env.render()
@@ -81,11 +104,11 @@ df=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_ppo, 'y_2': avg_rewards
 # df=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_ppo, 'y_2': avg_rewards_random, 'y_4': avg_rewards_fixed_1, 'y_5': avg_rewards_fixed_2})
 plt.xlabel("Time Slot")
 plt.ylabel("Time Average Cost")
-plt.plot( 'x', 'y_1', data=df, marker='o', markevery = 700, color='red', linewidth=1, label="ppo")
-plt.plot( 'x', 'y_2', data=df, marker='^', markevery = 700, color='olive', linewidth=1, label="random")
-plt.plot( 'x', 'y_3', data=df, marker='s', markevery = 700, color='cyan', linewidth=1, label="myopic")
-plt.plot( 'x', 'y_4', data=df, marker='*', markevery = 700, color='skyblue', linewidth=1, label="fixed 0.4kW")
-plt.plot( 'x', 'y_5', data=df, marker='+', markevery = 700, color='navy', linewidth=1, label="fixed 1kW")
+plt.plot( 'x', 'y_1', data=df, marker='o', markevery = 10, color='red', linewidth=1, label="ppo")
+plt.plot( 'x', 'y_2', data=df, marker='^', markevery = 10, color='olive', linewidth=1, label="random")
+plt.plot( 'x', 'y_3', data=df, marker='s', markevery = 10, color='cyan', linewidth=1, label="myopic")
+plt.plot( 'x', 'y_4', data=df, marker='*', markevery = 10, color='skyblue', linewidth=1, label="fixed 0.4kW")
+plt.plot( 'x', 'y_5', data=df, marker='+', markevery = 10, color='navy', linewidth=1, label="fixed 1kW")
 plt.legend()
 plt.grid()
 plt.show()
