1caff8fbc8e746eef9674e33441886d1a08ff157
haitt00 <54594085+haitt00@users.noreply.github.com>
2020-02-04 20:15:53 +0700

    .

--- agent.py
+++ agent.py
@@ -29,12 +29,12 @@ avg_rewards_fixed_2 = []
 obs = env.reset()
 t = 0
 t_range = 10000
-# for i in range(t_range):
-#     action = env.env_method('myopic_action_cal')
-#     obs, rewards, dones, info = env.step(action)
-#     rewards_list_myopic.append(1 / rewards)
-#     avg_rewards_myopic.append(np.mean(rewards_list_myopic[-1000:-1]))
-#     if dones: env.reset()
+for i in range(t_range):
+    action = env.env_method('myopic_action_cal')
+    obs, rewards, dones, info = env.step(action)
+    rewards_list_myopic.append(1 / rewards)
+    avg_rewards_myopic.append(np.mean(rewards_list_myopic[:]))
+    if dones: env.reset()
 
 for i in range(t_range):
     action = env.env_method('fixed_action_cal', 400)
@@ -68,6 +68,7 @@ for i in range(t_range):
     # env.render()
 print('--RESULTS--')
 print('{:15}{:30}'.format('method','time average cost'))
+print('{:15}{:<30}'.format('myopic',avg_rewards_myopic[-1]))
 print('{:15}{:<30}'.format('fixed 0.4kW',avg_rewards_fixed_1[-1]))
 print('{:15}{:<30}'.format('fixed 1kW',avg_rewards_fixed_2[-1]))
 print('{:15}{:<30}'.format('random',avg_rewards_random[-1]))
@@ -76,13 +77,13 @@ print('{:15}{:<30}'.format('PPO', avg_rewards_ppo[-1]))
 # print('{:>10.8} {:>10.8} {:>10.8} {:>10.8}'.format(, avg_rewards_fixed_2[-1], , avg_rewards_ppo[-1]))
 import matplotlib.pyplot as plt
 
-# df=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_ppo, 'y_2': avg_rewards_random, 'y_3': avg_rewards_myopic, 'y_4': avg_rewards_fixed_1, 'y_5': avg_rewards_fixed_2})
-df=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_ppo, 'y_2': avg_rewards_random, 'y_4': avg_rewards_fixed_1, 'y_5': avg_rewards_fixed_2})
+df=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_ppo, 'y_2': avg_rewards_random, 'y_3': avg_rewards_myopic, 'y_4': avg_rewards_fixed_1, 'y_5': avg_rewards_fixed_2})
+# df=pd.DataFrame({'x': range(t_range), 'y_1': avg_rewards_ppo, 'y_2': avg_rewards_random, 'y_4': avg_rewards_fixed_1, 'y_5': avg_rewards_fixed_2})
 plt.xlabel("Time Slot")
 plt.ylabel("Time Average Cost")
 plt.plot( 'x', 'y_1', data=df, marker='o', markevery = 700, color='red', linewidth=1, label="ppo")
 plt.plot( 'x', 'y_2', data=df, marker='^', markevery = 700, color='olive', linewidth=1, label="random")
-# plt.plot( 'x', 'y_3', data=df, marker='', color='lightblue', linewidth=1, label="fixed 0")
+plt.plot( 'x', 'y_3', data=df, marker='s', markevery = 700, color='cyan', linewidth=1, label="myopic")
 plt.plot( 'x', 'y_4', data=df, marker='*', markevery = 700, color='skyblue', linewidth=1, label="fixed 0.4kW")
 plt.plot( 'x', 'y_5', data=df, marker='+', markevery = 700, color='navy', linewidth=1, label="fixed 1kW")
 plt.legend()
