53915b85eeeb4aff4f95c26509c321e5d98825e8
Hai Trinh <hai.tt184255@sis.hust.edu.vn>
2020-04-04 14:17:53 +0700

    update plot 1kts

--- graph_plot.py
+++ graph_plot.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-my_path = os.path.abspath('res')
+my_res_path = os.path.abspath('res/1kts/')
+my_xls_path = os.path.abspath('res/')
 
 val = 0.5 # value of p
 
@@ -13,11 +14,13 @@ try:
     plot_list = [[] for _ in range(6)]
     i = 0
     rng = 0
-    with open(os.path.join(my_path, xls_file), 'rb') as f:
+    with open(os.path.join(my_xls_path, xls_file), 'rb') as f:
         xls_data = pd.read_excel(f)
         for i in range(6):
-            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()
+            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()[:1000]
+            # print(plot_list[i])
             rng = len(plot_list[i])
+            # rng = 
         i += 1
     df = pd.DataFrame(
         {'x': range(rng), 'y_1': plot_list[0], 'y_2': plot_list[1], 'y_3': plot_list[2], 'y_4': plot_list[3],
@@ -31,10 +34,10 @@ try:
              label="fixed 0.4kW")
     plt.plot('x', 'y_5', data=df, marker='+', markevery=int(rng / 10), color='navy', linewidth=1, label="fixed 1kW")
     plt.plot('x', 'y_6', data=df, marker='x', markevery=int(rng / 10), color='green', linewidth=1, label="q learning")
-    plt.legend()
+    plt.legend(fancybox=True, shadow=True)
     plt.grid()
     fig_file = 'p=' + str(val) + '/avg_total_p=' + str(val) + '_.png'
-    plt.savefig(os.path.join(my_path, fig_file))
+    plt.savefig(os.path.join(my_res_path, fig_file))
     plt.show()
 except IOError:
     print('Fuck')
@@ -46,10 +49,10 @@ try:
     plot_list = [[] for _ in range(6)]
     i = 0
     rng = 0
-    with open(os.path.join(my_path, xls_file), 'rb') as f:
+    with open(os.path.join(my_xls_path, xls_file), 'rb') as f:
         xls_data = pd.read_excel(f)
         for i in range(6):
-            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()
+            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()[:1000]
             rng = len(plot_list[i])
         i += 1
     df = pd.DataFrame(
@@ -64,10 +67,10 @@ try:
              label="fixed 0.4kW")
     plt.plot('x', 'y_5', data=df, marker='+', markevery=int(rng / 10), color='navy', linewidth=1, label="fixed 1kW")
     plt.plot('x', 'y_6', data=df, marker='x', markevery=int(rng / 10), color='green', linewidth=1, label="q learning")
-    plt.legend()
+    plt.legend(fancybox=True, shadow=True)
     plt.grid()
     fig_file = 'p=' + str(val) + '/avg_time_p=' + str(val) + '_.png'
-    plt.savefig(os.path.join(my_path, fig_file))
+    plt.savefig(os.path.join(my_res_path, fig_file))
     plt.show()
 except IOError:
     print('Fuck')
@@ -79,10 +82,10 @@ try:
     plot_list = [[] for _ in range(6)]
     i = 0
     rng = 0
-    with open(os.path.join(my_path, xls_file), 'rb') as f:
+    with open(os.path.join(my_xls_path, xls_file), 'rb') as f:
         xls_data = pd.read_excel(f)
         for i in range(6):
-            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()
+            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()[:1000]
             rng = len(plot_list[i])
         i += 1
     df = pd.DataFrame(
@@ -97,10 +100,10 @@ try:
              label="fixed 0.4kW")
     plt.plot('x', 'y_5', data=df, marker='+', markevery=int(rng / 10), color='navy', linewidth=1, label="fixed 1kW")
     plt.plot('x', 'y_6', data=df, marker='x', markevery=int(rng / 10), color='green', linewidth=1, label="q learning")
-    plt.legend()
+    plt.legend(fancybox=True, shadow=True)
     plt.grid()
     fig_file = 'p=' + str(val) + '/avg_backup_p=' + str(val) + '_.png'
-    plt.savefig(os.path.join(my_path, fig_file))
+    plt.savefig(os.path.join(my_res_path, fig_file))
     plt.show()
 except IOError:
     print('Fuck')
@@ -112,10 +115,10 @@ try:
     plot_list = [[] for _ in range(6)]
     i = 0
     rng = 0
-    with open(os.path.join(my_path, xls_file), 'rb') as f:
+    with open(os.path.join(my_xls_path, xls_file), 'rb') as f:
         xls_data = pd.read_excel(f)
         for i in range(6):
-            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()
+            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()[:1000]
             rng = len(plot_list[i])
         i += 1
     df = pd.DataFrame(
@@ -130,10 +133,10 @@ try:
              label="fixed 0.4kW")
     plt.plot('x', 'y_5', data=df, marker='+', markevery=int(rng / 10), color='navy', linewidth=1, label="fixed 1kW")
     plt.plot('x', 'y_6', data=df, marker='x', markevery=int(rng / 10), color='green', linewidth=1, label="q learning")
-    plt.legend()
+    plt.legend(fancybox=True, shadow=True)
     plt.grid()
     fig_file = 'p=' + str(val) + '/avg_battery_p=' + str(val) + '_.png'
-    plt.savefig(os.path.join(my_path, fig_file))
+    plt.savefig(os.path.join(my_res_path, fig_file))
     plt.show()
 except IOError:
     print('Fuck')
@@ -145,10 +148,10 @@ try:
     plot_list = [[] for _ in range(6)]
     i = 0
     rng = 0
-    with open(os.path.join(my_path, xls_file), 'rb') as f:
+    with open(os.path.join(my_xls_path, xls_file), 'rb') as f:
         xls_data = pd.read_excel(f)
         for i in range(6):
-            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()
+            plot_list[i] = xls_data['y_' + str(i + 1)].tolist()[:1000]
             rng = len(plot_list[i])
         i += 1
     df = pd.DataFrame(
@@ -163,10 +166,10 @@ try:
              label="fixed 0.4kW")
     plt.plot('x', 'y_5', data=df, marker='+', markevery=int(rng / 10), color='navy', linewidth=1, label="fixed 1kW")
     plt.plot('x', 'y_6', data=df, marker='x', markevery=int(rng / 10), color='green', linewidth=1, label="q learning")
-    plt.legend()
+    plt.legend(fancybox=True, shadow=True)
     plt.grid()
     fig_file = 'p=' + str(val) + '/avg_energy_p=' + str(val) + '_.png'
-    plt.savefig(os.path.join(my_path, fig_file))
+    plt.savefig(os.path.join(my_res_path, fig_file))
     plt.show()
 except IOError:
     print('Fuck')
@@ -178,24 +181,28 @@ i = 0
 rng = 0
 for name in xls_file_names:
     try:
-        with open(os.path.join(my_path, name), 'rb') as f:
+        with open(os.path.join(my_xls_path, name), 'rb') as f:
             xls_data = pd.read_excel(f)
-            plot_list[i] = xls_data['y_1'].tolist()
+            plot_list[i] = xls_data['y_1'].tolist()[:1000]
             rng = len(plot_list[i])
             i += 1
     except IOError:
         print('Fuck')
 
 xx = range(rng)
-fig = plt.stackplot(xx, plot_list, colors = 'w', edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
+fig = plt.stackplot(xx, plot_list, edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
 hatches = ['...', '+++++', '///']
 for s, h in zip(fig, hatches):
     s.set_hatch(h)
 plt.title('PPO')
-plt.legend()
+plt.xlabel("Time Slot")
+plt.ylabel("Time Average Cost")
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, 0.), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.grid()
 my_file = 'p='+str(val)+'/ppo_area'+'p='+str(val)+'.png'
-plt.savefig(os.path.join(my_path, my_file))
+plt.savefig(os.path.join(my_res_path, my_file))
 plt.show()
 
 # random area chart
@@ -205,24 +212,28 @@ i = 0
 rng = 0
 for name in xls_file_names:
     try:
-        with open(os.path.join(my_path, name), 'rb') as f:
+        with open(os.path.join(my_xls_path, name), 'rb') as f:
             xls_data = pd.read_excel(f)
-            plot_list[i] = xls_data['y_2'].tolist()
+            plot_list[i] = xls_data['y_2'].tolist()[:1000]
             rng = len(plot_list[i])
             i += 1
     except IOError:
         print('Fuck')
 
 xx = range(rng)
-fig = plt.stackplot(xx, plot_list, colors = 'w', edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
+fig = plt.stackplot(xx, plot_list, edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
 hatches = ['...', '+++++', '///']
 for s, h in zip(fig, hatches):
     s.set_hatch(h)
 plt.title('Random')
-plt.legend()
+plt.xlabel("Time Slot")
+plt.ylabel("Time Average Cost")
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, 0.), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.grid()
 my_file = 'p='+str(val)+'/random_area'+'p='+str(val)+'.png'
-plt.savefig(os.path.join(my_path, my_file))
+plt.savefig(os.path.join(my_res_path, my_file))
 plt.show()
 
 # myopic area chart
@@ -232,24 +243,28 @@ i = 0
 rng = 0
 for name in xls_file_names:
     try:
-        with open(os.path.join(my_path, name), 'rb') as f:
+        with open(os.path.join(my_xls_path, name), 'rb') as f:
             xls_data = pd.read_excel(f)
-            plot_list[i] = xls_data['y_3'].tolist()
+            plot_list[i] = xls_data['y_3'].tolist()[:1000]
             rng = len(plot_list[i])
             i += 1
     except IOError:
         print('Fuck')
 
 xx = range(rng)
-fig = plt.stackplot(xx, plot_list, colors = 'w', edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
+fig = plt.stackplot(xx, plot_list, edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
 hatches = ['...', '+++++', '///']
 for s, h in zip(fig, hatches):
     s.set_hatch(h)
 plt.title('Myopic')
-plt.legend()
+plt.xlabel("Time Slot")
+plt.ylabel("Time Average Cost")
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, 0.), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.grid()
 my_file = 'p='+str(val)+'/myopic_area'+'p='+str(val)+'.png'
-plt.savefig(os.path.join(my_path, my_file))
+plt.savefig(os.path.join(my_res_path, my_file))
 plt.show()
 
 # fixed 1 area chart
@@ -259,24 +274,28 @@ i = 0
 rng = 0
 for name in xls_file_names:
     try:
-        with open(os.path.join(my_path, name), 'rb') as f:
+        with open(os.path.join(my_xls_path, name), 'rb') as f:
             xls_data = pd.read_excel(f)
-            plot_list[i] = xls_data['y_4'].tolist()
+            plot_list[i] = xls_data['y_4'].tolist()[:1000]
             rng = len(plot_list[i])
             i += 1
     except IOError:
         print('Fuck')
 
 xx = range(rng)
-fig = plt.stackplot(xx, plot_list, colors = 'w', edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
+fig = plt.stackplot(xx, plot_list, edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
 hatches = ['...', '+++++', '///']
 for s, h in zip(fig, hatches):
     s.set_hatch(h)
 plt.title('Fixed 0.4kW')
-plt.legend()
+plt.xlabel("Time Slot")
+plt.ylabel("Time Average Cost")
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, 0.), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.grid()
 my_file = 'p='+str(val)+'/fixed_0.4kW_area'+'p='+str(val)+'.png'
-plt.savefig(os.path.join(my_path, my_file))
+plt.savefig(os.path.join(my_res_path, my_file))
 plt.show()
 
 # fixed 2 area chart
@@ -286,24 +305,28 @@ i = 0
 rng = 0
 for name in xls_file_names:
     try:
-        with open(os.path.join(my_path, name), 'rb') as f:
+        with open(os.path.join(my_xls_path, name), 'rb') as f:
             xls_data = pd.read_excel(f)
-            plot_list[i] = xls_data['y_5'].tolist()
+            plot_list[i] = xls_data['y_5'].tolist()[:1000]
             rng = len(plot_list[i])
             i += 1
     except IOError:
         print('Fuck')
 
 xx = range(rng)
-fig = plt.stackplot(xx, plot_list, colors = 'w', edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
+fig = plt.stackplot(xx, plot_list, edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
 hatches = ['...', '+++++', '///']
 for s, h in zip(fig, hatches):
     s.set_hatch(h)
 plt.title('Fixed 1kW')
-plt.legend()
+plt.xlabel("Time Slot")
+plt.ylabel("Time Average Cost")
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, 0.), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True)
+# plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.grid()
 my_file = 'p='+str(val)+'/fixed_1kW_area'+'p='+str(val)+'.png'
-plt.savefig(os.path.join(my_path, my_file))
+plt.savefig(os.path.join(my_res_path, my_file))
 plt.show()
 
 # dqn area chart
@@ -313,22 +336,25 @@ i = 0
 rng = 0
 for name in xls_file_names:
     try:
-        with open(os.path.join(my_path, name), 'rb') as f:
+        with open(os.path.join(my_xls_path, name), 'rb') as f:
             xls_data = pd.read_excel(f)
-            plot_list[i] = xls_data['y_6'].tolist()
+            plot_list[i] = xls_data['y_6'].tolist()[:1000]
             rng = len(plot_list[i])
             i += 1
     except IOError:
         print('Fuck')
 
 xx = range(rng)
-fig = plt.stackplot(xx, plot_list, colors = 'w', edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
+fig = plt.stackplot(xx, plot_list, edgecolor = 'black', labels = ['Delay cost', 'Backup cost', 'Battery cost'])
 hatches = ['...', '+++++', '///']
 for s, h in zip(fig, hatches):
     s.set_hatch(h)
 plt.title('DQN')
-plt.legend()
+plt.xlabel("Time Slot")
+plt.ylabel("Time Average Cost")
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, 0.), ncol=3, fancybox=True, shadow=True)
+# lgd = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.grid()
 my_file = 'p='+str(val)+'/dqn_area'+'p='+str(val)+'.png'
-plt.savefig(os.path.join(my_path, my_file))
+plt.savefig(os.path.join(my_res_path, my_file))
 plt.show()
